aa5d40a7fb85a03d28e4c2c2e83dbf9b99709eb9
Tobias Winkler <tobias.winkler@evan.team>
2019-10-25 16:28:21 +0200

    fix `serve-build` `gulp.series` job
    - [CORE-457]

--- gulp/build.js
+++ gulp/build.js
@@ -82,7 +82,7 @@ gulp.task('bundle:vendor', function() {
   .pipe(gulp.dest('src/build'));
 })
 
-gulp.task('bundle:js', gulp.series(['bundle:vendor']), function() {
+gulp.task('bundle:js', gulp.series(['bundle:vendor'], function() {
   var builder = new Builder('.', 'systemjs.config.js');
 
   return builder
@@ -127,7 +127,7 @@ gulp.task('bundle:js', gulp.series(['bundle:vendor']), function() {
     .catch(function(err) {
       console.error('>>> [systemjs-builder] Bundling failed'.bold.green, err);
     })
-});
+}));
 
 // Compile TypeScript to JS
 gulp.task('compile:ts', function () {
