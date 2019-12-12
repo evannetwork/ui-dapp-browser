573957232006e468e5ec9dd679cbf58289bf1287
Tschuck <tobias.winkler@evan.team>
2019-12-12 09:32:36 +0100

    use classlist instead of className append
    - [CORE-748]

--- src/app/loading.ts
+++ src/app/loading.ts
@@ -33,7 +33,7 @@ export function finishDAppLoading()  {
 
   if (initialLoading && initialLoading.className.indexOf('hidden') === -1) {
     utils.raiseProgress(10);
-    initialLoading.className += ' hidden';
+    initialLoading.classList.add('hidden');
 
     setTimeout(() => {
       // don't remove it, when another function call was started before
